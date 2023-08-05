1db719f3113e2d8c5b900df0679c1c91ed0eff1a
Spacew1se <crod201@gmail.com>
2023-08-04 23:10:12 -0700

    Change calculator display by pushing buttons
    Numbers and operators will display when clicked with logic.
    e.g. clicking two operators in a row will not display the second clicked
    Store the button clicked by the user and the display text
    Clicking the clear button will change display to 0 and erase stored values

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 let num1, num2, operator;
-
+let btnPressed, prevPressed;
+const operators = ['+', '-', 'x', '/'];
+//Store operands and operator as object - key:value pair
 
 function add(num1, num2) {
     return num1 + num2;
@@ -26,7 +28,34 @@ function operate(num1, num2, operator) {
     }
 }
 
-console.log(operate(2, 2, '+'))
-console.log(operate(2, 2, '-'))
-console.log(operate(2, 2, '*'))
-console.log(operate(2, 2, '/'))
+function displayNumbers() {
+    const buttons = document.querySelectorAll('.digit, .operator');
+    const display = document.querySelector('.display');
+    buttons.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            btnPressed = e.target.textContent
+            if (display.textContent === '0' && operators.includes(btnPressed) === false) {
+                display.textContent = e.target.textContent;
+            }
+            else if ((operators.includes(prevPressed) && operators.includes(btnPressed)) === false) {
+                display.textContent += e.target.textContent;
+            }
+            prevPressed = btnPressed
+            displayValue = display.textContent;        
+        }); 
+    }); 
+}
+
+function clearDisplay() {
+    const clear = document.querySelector('.clear')
+    const display = document.querySelector('.display');
+    clear.addEventListener('click', (e) => {
+        display.textContent = '0';
+        displayValue = display.textContent;
+        btnPressed = '';
+        prevPressed = '';
+    })
+}
+
+displayNumbers()
+clearDisplay()
