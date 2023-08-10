1b48a4087ca1bd3bd36cc455444192e30b1032a2
Spacew1se <crod201@gmail.com>
2023-08-10 12:05:26 -0700

    Improve equals and backspace functions
    Add a previous operator variable to fix bug in backspace
    Expand regex in equals function to remove more cases of trailing zeroes
    Eliminate unecessary logic checks in digit display function

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ const operation = {
     input1: null,
     input2: null,
     operator: null,
+    prevoperator: null
 };
 
 function add(num1, num2) {
@@ -16,7 +17,7 @@ function subtract(num1, num2) {
 }
 
 function multiply(num1, num2) {
-    let rounded = Math.round((num1 / num2) * 1000000000) / 1000000000
+    let rounded = Math.round((num1 * num2) * 1000000) / 1000000;
     rounded = "" + rounded;
     rounded = rounded.replace(/[.]0+$/g, '')
     return rounded;
@@ -32,7 +33,7 @@ function divide(num1, num2) {
         return errormsg;
     }
 
-    let rounded = Math.round((num1 / num2) * 1000000000) / 1000000000
+    let rounded = Math.round((num1 / num2) * 1000000) / 1000000;
     rounded = "" + rounded;
     rounded = rounded.replace(/[.]0+$/g, '')
     return rounded;
@@ -53,31 +54,31 @@ function displayNumbers() {
     buttons.forEach(btn => {
         btn.addEventListener('click', (e) => {
             const btnPressed = e.target.textContent
- 
-            if (operation.input1 === null) {
-                if (display.textContent.endsWith('0.')) {
+                if (!operation.input1 || display.textContent === '0') {
+                    if (display.textContent.endsWith('0.')) {
+                        display.textContent += btnPressed;
+                    }
+                    else {
+                        display.textContent = btnPressed;
+                    }
+                    operation.input1 = display.textContent;
+                    equation.textContent = operation.input1;
+                }         
+                else if (!operation.operator) {
                     display.textContent += btnPressed;
+                    operation.input1 = display.textContent;
+                    equation.textContent += btnPressed;
                 }
                 else {
-                    display.textContent = btnPressed;
+                    display.textContent = (!operation.input2 || display.textContent === '0') ? btnPressed : display.textContent + btnPressed;
+                    operation.input2 = display.textContent;
+                    equation.textContent += btnPressed
                 }
-                operation.input1 = display.textContent;
-                equation.textContent = operation.input1;
-            }         
-            else if (operation.operator === null) {
-                display.textContent += btnPressed;
-                operation.input1 = display.textContent;
-                equation.textContent += btnPressed;
-            }
-            else {
-                display.textContent = operation.input2 === null ? btnPressed : display.textContent + btnPressed;
-                operation.input2 = display.textContent;
-                equation.textContent += btnPressed
-            }
         }); 
     }); 
 }
 
+//Show Zero in equation
 function decimalButton() {
     const decbtn = document.querySelector('.decimal');
     decbtn.addEventListener('click', (e) => {
@@ -91,11 +92,13 @@ function decimalButton() {
     });
 }
 
+
+
 function opButtons() {
     const opButtons = document.querySelectorAll('.operator');
     opButtons.forEach(btn => {
         btn.addEventListener('click', (e) => {
-            if (operation.input2 !== null) {
+            if (operation.input2) {
                 display.textContent = operate(Number(operation.input1), Number(operation.input2), operation.operator);    
                 operation.input2 = null;
             }
@@ -120,12 +123,12 @@ function clearDisplay() {
 function equals() {
     const equals = document.querySelector('.equals');
     equals.addEventListener('click', (e) => {
-        if (operation.operator === null) {
-            display.textContent = display.textContent.replace(/[.]$|[.]0+$/g, '');
+        if (!operation.operator) {
+            display.textContent = display.textContent.replace(/(^-?\d+\.\d*[1-9])(0+$)|(\.0+$)/g, "$1");
             operation.input1 = display.textContent;
             equation.textContent = operation.input1 + e.target.textContent;
         }
-        else if (operation.input2 === null) {
+        else if (!operation.input2) {
             equation.textContent = operation.input1 + operation.operator + operation.input1 + e.target.textContent;
             display.textContent = operate(Number(operation.input1), Number(operation.input1), operation.operator);
         }
@@ -133,12 +136,28 @@ function equals() {
             equation.textContent = operation.input1 + operation.operator + operation.input2 + e.target.textContent;
             display.textContent = operate(Number(operation.input1), Number(operation.input2), operation.operator);
         }
+        operation.prevoperator = '=';
         operation.input1 = null;
         operation.input2 = null;
         operation.operator = null;
     });
 }
 
+//Fix backspace to properly use prevoperator
+function backspace() {
+    const del = document.querySelector('.backspace');
+    del.addEventListener('click', (e) => {
+        if (display.textContent.length > 1 && operation.prevoperator !== '=') {
+            display.textContent = display.textContent.slice(0, -1)
+        }
+        else {
+            display.textContent = '0';
+        }
+        equation.textContent = display.textContent;
+    })
+}
+
+backspace()
 displayNumbers()
 decimalButton()
 opButtons()
