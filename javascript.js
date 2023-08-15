8501aa0b64a115d5ca93c3f4ed6dc66a62fbd81c
Spacew1se <crod201@gmail.com>
2023-08-14 22:05:18 -0700

    Fix bugs in decimal, backspace, and display

--- javascript.js
+++ javascript.js
@@ -5,7 +5,6 @@ const operation = {
     input1: null,
     input2: null,
     operator: null,
-    prevoperator: null
 };
 
 function add(num1, num2) {
@@ -41,10 +40,10 @@ function divide(num1, num2) {
 
 function operate(num1, num2, op) {
     switch (op) {
-        case '+': return add(num1, num2);
-        case '-': return subtract(num1, num2);
-        case '*': return multiply(num1, num2);
-        case '/': return divide(num1, num2);
+        case ' + ': return add(num1, num2);
+        case ' - ': return subtract(num1, num2);
+        case ' * ': return multiply(num1, num2);
+        case ' / ': return divide(num1, num2);
     }
 }
 
@@ -54,45 +53,49 @@ function displayNumbers() {
     buttons.forEach(btn => {
         btn.addEventListener('click', (e) => {
             const btnPressed = e.target.textContent
-                if (!operation.input1 || display.textContent === '0') {
-                    if (display.textContent.endsWith('0.')) {
-                        display.textContent += btnPressed;
-                    }
-                    else {
+                if (!operation.input1) {
+                    if (btnPressed !== '0') {
                         display.textContent = btnPressed;
-                    }
-                    operation.input1 = display.textContent;
-                    equation.textContent = operation.input1;
+                        operation.input1 = display.textContent;
+                    } 
                 }         
                 else if (!operation.operator) {
                     display.textContent += btnPressed;
                     operation.input1 = display.textContent;
-                    equation.textContent += btnPressed;
                 }
+                else if (!operation.input2) {
+                        display.textContent = btnPressed;
+                        operation.input2 = display.textContent;
+                    }
                 else {
-                    display.textContent = (!operation.input2 || display.textContent === '0') ? btnPressed : display.textContent + btnPressed;
+                    if (btnPressed !== '0') {
+                        display.textContent += btnPressed;
+                    }  
                     operation.input2 = display.textContent;
-                    equation.textContent += btnPressed
                 }
         }); 
     }); 
 }
 
-//Show Zero in equation
 function decimalButton() {
     const decbtn = document.querySelector('.decimal');
     decbtn.addEventListener('click', (e) => {
-        if (!display.textContent.includes('.') && (operation.input1 || display.textContent === '0')) {
-            if (operation.input1 && operation.operator && !operation.input2) {}
-            else {
-                display.textContent += e.target.textContent;
-                equation.textContent += e.target.textContent;
-            }    
+        if (!operation.input1) {
+            display.textContent = '0.';
+            operation.input1 = display.textContent;
+            equation.textContent = '';
         }
+        if (display.textContent.includes('.')) {
+            if (operation.input1 && operation.operator && !operation.input2) {
+                display.textContent = '0.';
+                operation.input2 = display.textContent;
+            }  
+        }
+        else {
+            display.textContent += e.target.textContent;
+        }        
     });
 }
-
-
 
 function opButtons() {
     const opButtons = document.querySelectorAll('.operator');
@@ -101,6 +104,9 @@ function opButtons() {
             if (operation.input2) {
                 display.textContent = operate(Number(operation.input1), Number(operation.input2), operation.operator);    
                 operation.input2 = null;
+            }
+            if (operation.input1) {
+                display.textContent = display.textContent.replace(/(^-?\d+\.\d*[1-9])(0+$)|(\.0+$)|(\.$)/g, "$1");
             }
             operation.input1 = display.textContent;
             operation.operator = e.target.textContent;
@@ -124,19 +130,21 @@ function equals() {
     const equals = document.querySelector('.equals');
     equals.addEventListener('click', (e) => {
         if (!operation.operator) {
-            display.textContent = display.textContent.replace(/(^-?\d+\.\d*[1-9])(0+$)|(\.0+$)/g, "$1");
+            display.textContent = display.textContent.replace(/(^-?\d+\.\d*[1-9])(0+$)|(\.0+$)|(\.$)/g, "$1");
             operation.input1 = display.textContent;
             equation.textContent = operation.input1 + e.target.textContent;
         }
         else if (!operation.input2) {
+            console.log(operation)
             equation.textContent = operation.input1 + operation.operator + operation.input1 + e.target.textContent;
             display.textContent = operate(Number(operation.input1), Number(operation.input1), operation.operator);
         }
         else {
+            console.log(operation)
+            operation.input2 = operation.input2.replace(/(^-?\d+\.\d*[1-9])(0+$)|(\.0+$)/g, "$1");
             equation.textContent = operation.input1 + operation.operator + operation.input2 + e.target.textContent;
             display.textContent = operate(Number(operation.input1), Number(operation.input2), operation.operator);
         }
-        operation.prevoperator = '=';
         operation.input1 = null;
         operation.input2 = null;
         operation.operator = null;
@@ -147,13 +155,9 @@ function equals() {
 function backspace() {
     const del = document.querySelector('.backspace');
     del.addEventListener('click', (e) => {
-        if (display.textContent.length > 1 && operation.prevoperator !== '=') {
+        if (display.textContent.length > 1 && (operation.input1 || operation.input2)) {
             display.textContent = display.textContent.slice(0, -1)
         }
-        else {
-            display.textContent = '0';
-        }
-        equation.textContent = display.textContent;
     })
 }
 
